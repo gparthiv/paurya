ee6f966aa626af34c7ba6e6430bfd28274282acd
Parthiv Ghosh <g.parthiv119@gmail.com>
2026-05-22 11:22:52 +0000

    initial Dashboard built with mock data

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,26 @@
+import IssueCard from "../components/cards/IssueCard";
+import mockIssues from "../data/mockIssues";
+
 function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="p-6">
+
+      <section>
+        <div className="space-y-4">
+          {mockIssues.map((issue) => (
+            <IssueCard
+              key={issue.id}
+              title={issue.title}
+              area={issue.area}
+              severity={issue.severity}
+              status={issue.status}
+            />
+          ))}
+        </div>
+      </section>
+
+    </div>
+  );
 }
 
 export default Dashboard;
